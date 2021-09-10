28ae9bb7063e8677f98fcda4f1ef5991c23f4a0b
Thiago Mota <thiagomota99@outlook.com>
2021-09-10 02:02:59 -0300

    Organizando a Aplicação em SubModulos

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { PhotoModule } from '../photo/photo.module';
+import { FilterByDescriptionPipe } from './filter-by-description.pipe';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotosComponent } from './photos/photos.component';
+
+@NgModule({
+    declarations: [
+        PhotoListComponent,
+        PhotosComponent,
+        LoadButtonComponent,
+        FilterByDescriptionPipe,        
+    ],
+    imports: [ 
+        CommonModule, //CommomModule possui as diretivas do Angular para que possamos utiliza-las em nossos módulos de feature
+        PhotoModule,
+        HttpClientModule, //Colocando o import do HttpClient, onde o mesmo fornece um provider configurado que sabe criar um objeto do tipo HttpClient
+    ] 
+})
+export class PhotoListModule {
+
+}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,27 +1,16 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 
 @NgModule({
-    declarations: [ 
-        PhotoComponent, //Declarando componente em um módulo
-        PhotoListComponent, //Declarando componente em um módulo
-        PhotoFormComponent, //Declarando componente em um módulo
-        PhotosComponent, //Declarando componente em um módulo
-        LoadButtonComponent, //Declarando componente em um módulo
-        FilterByDescriptionPipe,  //Declarando filter em um módulo
-    ],
+    declarations: [],
     imports: [
-        HttpClientModule, //Colocando o import do HttpClient, onde o mesmo fornece um provider configurado que sabe criar um objeto do tipo HttpClient
-        CommonModule //CommomModule possui as diretivas do Angular para que possamos utiliza-las em nossos módulos de feature
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
     ],
 })
 export class PhotosModule {
