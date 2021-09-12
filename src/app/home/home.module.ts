5f7d8853e9616f336c4430d8c7c06ed2f75470f5
Thiago Mota <thiagomota99@outlook.com>
2021-09-12 17:48:37 -0300

    Componentizando a mensagem de erro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { SignInComponent } from './sigin/signin.component';
 
 @NgModule({
-    declarations: [SignInComponent],
-    imports: [CommonModule, ReactiveFormsModule]
+    declarations: [ SignInComponent ],
+    imports: [
+        CommonModule, 
+        ReactiveFormsModule,
+        VMessageModule,
+    ]
 })
 export class HomeModule { }
