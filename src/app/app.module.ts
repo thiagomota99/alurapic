107ee88f0eb055bfb0ea3ce1109540016d338b9f
Thiago Mota <thiagomota99@outlook.com>
2021-09-12 21:05:45 -0300

    Criando o HeaderComponent
    Criando o cabeçalho da página.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { CoreModule } from './core/core.module';
 import { ErrorsModule } from './errors/errors.module';
 import { HomeModule } from './home/home.module';
 import { PhotosModule } from './photos/photos.module';
@@ -16,6 +17,7 @@ import { PhotosModule } from './photos/photos.module';
     PhotosModule,
     ErrorsModule,
     HomeModule,
+    CoreModule,
     AppRoutingModule, //Importando o módulo de rotas raízes da aplicação 
   ],
   providers: [],
