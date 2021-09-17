5619e4bc0257fc47e5001fe56284f28dfe341cc5
Thiago Mota <thiagomota99@outlook.com>
2021-09-17 03:16:25 -0300

    Criando template do PhotoFormComponent

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 import { PhotoFormComponent } from './photo-form.component';
 
 @NgModule({
     declarations: [ PhotoFormComponent ],
-    imports: [ CommonModule ]
+    imports: [ 
+        CommonModule,
+        FormsModule,
+        VMessageModule,
+        ReactiveFormsModule,
+    ]
 })
 export class PhotoFormModule {
 
